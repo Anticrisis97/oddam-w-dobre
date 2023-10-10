6c19f9a4de09b8bbfe21f5ee73a5144c33ea22a2
Patryk Pawlina <135826190+Anticrisis97@users.noreply.github.com>
2023-10-10 12:53:32 +0200

    form fix

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -11,21 +11,40 @@ function ContactForm () {
                 <p>Skontaktuj się z nami</p>
                 <img src={decoration} alt='decoration'></img>
                 <div className='name_and_email_container'>
+                    <form onSubmit={}>
                 <div className='name_and_email_container1'>
                     <label htmlFor="username">Wpisz swoje imię</label>
-                    <input type="text" id="username" name="username" placeholder='Krzysztof' />
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder='Krzysztof' />
                     <hr></hr>
                 </div>
                 <div className='name_and_email_container2'>
                     <label htmlFor="email">Wpisz swój email:</label>
-                    <input type="text" id="email" name="email" placeholder='abc@xyz.pl' />
+                    <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        placeholder='abc@xyz.pl' />
                     <hr></hr>
                 </div>
                     <div className='msg_container'>
                         <label htmlFor="messeage">Wpisz swoją wiadomość:</label>
-                        <textarea id="messeage" name="messeage" placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'/>
+                        <textarea
+                            id="messeage"
+                            name="messeage"
+                            placeholder='Lorem ipsum dolor sit amet,
+                             consectetur adipiscing elit, sed do
+                             eiusmod tempor incididunt ut labore et
+                             dolore magna aliqua. Ut enim ad minim
+                             veniam, quis nostrud exercitation ullamco
+                             laboris nisi ut aliquip ex ea commodo
+                             consequat.'/>
                         <hr></hr>
                     </div>
+                </form>
                 </div>
                 <div className='send_button'>
                     <button type="button" id="sendButton" className="send-button">Wyślij</button>
--- src/components/Zaloguj.js
+++ src/components/Zaloguj.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { useState } from 'react';
 
 function Zaloguj() {
+    const [loginData, setLoginData] = useState({ login: '', password: '' });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setLoginData({ ...loginData, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Wprowadzone dane:', loginData);
+    };
+
     return (
         <div>
-            <h1>456</h1>
+            <h2>Formularz Logowania</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="login">Login:</label>
+                    <input
+                        type="text"
+                        id="login"
+                        name="login"
+                        value={loginData.login}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Hasło:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={loginData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <button type="submit">Zaloguj się</button>
+                </div>
+            </form>
         </div>
     );
 }
