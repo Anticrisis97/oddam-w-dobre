4bde47aae67f65fdbb603908ac89fed24cfc6879
Patryk Pawlina <p4trykpawlina@gmail.com>
2023-10-11 15:19:22 +0200

    name input validation successfully implemented

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,79 +1,95 @@
-import '../css/contactform.css'
-import decoration from '../assets/Decoration.svg'
+import React, { useState } from 'react';
+import '../css/contactform.css';
+import decoration from '../assets/Decoration.svg';
 
+function ContactForm() {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        message: '',
+    });
 
+    const [error, setError] = useState('');
+    const [submitted, setSubmitted] = useState(false);
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
 
-function ContactForm () {
+        if (submitted && name === "username" && value.length < 1) {
+            setError('XYZ 123');
+        } else {
+            setError('');
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmitted(true);
+
+        if (formData.username.length < 1) {
+            setError('XYZ 123');
+        } else {
+            setError('');
+            // Obsługa przesłania formularza
+            // Przykład: console.log(formData);
+        }
+    };
+
     return (
         <div className='contact_form_main_container'>
             <div className='contact_form_container'>
                 <p>Skontaktuj się z nami</p>
                 <img src={decoration} alt='decoration'></img>
                 <div className='name_and_email_container'>
-                    <form onSubmit={}>
-                <div className='name_and_email_container1'>
-                    <label htmlFor="username">Wpisz swoje imię</label>
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        placeholder='Krzysztof' />
-                    <hr></hr>
-                </div>
-                <div className='name_and_email_container2'>
-                    <label htmlFor="email">Wpisz swój email:</label>
-                    <input
-                        type="text"
-                        id="email"
-                        name="email"
-                        placeholder='abc@xyz.pl' />
-                    <hr></hr>
-                </div>
-                    <div className='msg_container'>
-                        <label htmlFor="messeage">Wpisz swoją wiadomość:</label>
-                        <textarea
-                            id="messeage"
-                            name="messeage"
-                            placeholder='Lorem ipsum dolor sit amet,
-                             consectetur adipiscing elit, sed do
-                             eiusmod tempor incididunt ut labore et
-                             dolore magna aliqua. Ut enim ad minim
-                             veniam, quis nostrud exercitation ullamco
-                             laboris nisi ut aliquip ex ea commodo
-                             consequat.'/>
-                        <hr></hr>
-                    </div>
-                </form>
-                </div>
-                <div className='send_button'>
-                    <button type="button" id="sendButton" className="send-button">Wyślij</button>
+                    <form onSubmit={handleSubmit}>
+                        <div className='name_and_email_container1'>
+                            <label htmlFor="username">Wpisz swoje imię</label>
+                            <input
+                                type="text"
+                                id="username"
+                                name="username"
+                                placeholder='Krzysztof'
+                                value={formData.username}
+                                onChange={handleChange}
+                            />
+                            <hr></hr>
+                            {submitted && error && <p>{error}</p>}
+                        </div>
+                        <div className='name_and_email_container2'>
+                            <label htmlFor="email">Wpisz swój email:</label>
+                            <input
+                                type="text"
+                                id="email"
+                                name="email"
+                                placeholder='abc@xyz.pl'
+                                value={formData.email}
+                                onChange={handleChange}
+                            />
+                            <hr></hr>
+                        </div>
+                        <div className='msg_container'>
+                            <label htmlFor="message">Wpisz swoją wiadomość:</label>
+                            <textarea
+                                id="message"
+                                name="message"
+                                placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
+                                value={formData.message}
+                                onChange={handleChange}
+                            />
+                            <hr></hr>
+                        </div>
+                        <div className='send_button'>
+                            <button type="submit" id="sendButton" className="send-button">Wyślij</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-/*
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Skrypt działa");
-    const form = document.querySelector('form'); // Wybierz formularz, nie pole tekstowe
-    const usernameInput = document.getElementById('username'); // Poprawne id bez #
-
-    form.addEventListener('submit', function (event) {
-        const usernameValue = usernameInput.value;
-
-        if (!containsOnlyOneWord(usernameValue)) {
-            alert('XYZ XYZ');
-            event.preventDefault();
-        }
-    });
-
-    function containsOnlyOneWord(value) {
-        const words = value.trim().split(' ');
-        return words.length === 1;
-    }
-}); */
-
-
-export default ContactForm
+export default ContactForm;
