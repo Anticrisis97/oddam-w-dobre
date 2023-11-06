a266f5b3b42ce2d05d58588f3d6c0603fabb0e2c
Patryk Pawlina <p4trykpawlina@gmail.com>
2023-11-06 19:02:12 +0100

    abc

--- src/App.js
+++ src/App.js
@@ -1,55 +1,63 @@
 import React from 'react';
-import {BrowserRouter as Link, Route, Routes, useLocation} from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './components/Home';
-import Login from './components/Login'
+import Login from './components/Login';
 import CreateAccount from "./components/CreateAccount";
-import './css/main.css'
-import './css/yellowstripe.css'
-import './css/4stepssection.css'
-import './css/aboutus.css'
-import './css/whodowehelp.css'
-import './css/contactform.css'
-import antiquebox from './assets/Home-Hero-Image.jpg'
 import Logout from "./components/Logout";
-
+import './css/main.css';
+import './css/yellowstripe.css';
+import './css/4stepssection.css';
+import './css/aboutus.css';
+import './css/whodowehelp.css';
+import './css/contactform.css';
+import antiquebox from './assets/Home-Hero-Image.jpg';
 
 function App() {
-    const path = useLocation();
-    console.log(path)
-  return (
-      <div className="main_container">
-      <>
-      <nav>
-          <ul className="login_register">
-              <li>
-                  <Link to="/zaloguj">Zaloguj</Link>
-              </li>
-              <li>
-                  <Link to="/zalozkonto" className="styled_border1">Załóż konto</Link>
-              </li>
-          </ul>
-          <ul className="main_menu">
-              <li>
-                  <Link to="/" className="styled_border2">Start</Link>
-              </li>
-              <li>O co chodzi?</li>
-              <li>O nas</li>
-              <li>Fundacja i organizacje</li>
-              <li>Kontakt</li>
-          </ul>
-          <div className={path.pathname !== "/" && "top_pic"}>
-          <img className="top_picc" src={antiquebox} alt="antiques_box"></img>
-          </div>
-          </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-            <Route path="/zaloguj" element={<Login />} />
-            <Route path="/zalozkonto" element={<CreateAccount />} />
-            <Route path="/wyloguj" element={<Logout /> } />
-        </Routes>
-          </>
-      </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="main_container">
+                <Navigation />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/zaloguj" element={<Login />} />
+                    <Route path="/zalozkonto" element={<CreateAccount />} />
+                    <Route path="/wyloguj" element={<Logout />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+// Navigation component to use useLocation
+function Navigation() {
+    let location = useLocation();
+
+    return (
+        <>
+            <nav>
+                <ul className="login_register">
+                    <li>
+                        <Link to="/zaloguj">Zaloguj</Link>
+                    </li>
+                    <li>
+                        <Link to="/zalozkonto" className="styled_border1">Załóż konto</Link>
+                    </li>
+                </ul>
+                <ul className="main_menu">
+                    <li>
+                        <Link to="/" className="styled_border2">Start</Link>
+                    </li>
+                    <li>O co chodzi?</li>
+                    <li>O nas</li>
+                    <li>Fundacja i organizacje</li>
+                    <li>Kontakt</li>
+                </ul>
+                <div className={location.pathname !== "/" ? "top_pic" : ""}>
+                    <img className="top_picc" src={antiquebox} alt="antiques_box" />
+                </div>
+            </nav>
+        </>
+    );
 }
 
 export default App;
